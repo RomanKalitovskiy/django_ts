4aad9e44b53d39de23891f070d2ad317747764e4
Roman Kalitovskiy <roman.kalitovskyi@gmail.com>
2022-07-24 21:18:06 +0300

    testing actions

--- frontend/src/App/App.test.tsx
+++ frontend/src/App/App.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
+  // testing
   render(<App />);
   const linkElement = screen.getByText(/test/i);
   expect(linkElement).toBeInTheDocument();

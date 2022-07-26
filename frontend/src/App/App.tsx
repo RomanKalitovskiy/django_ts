1e114093f038d9442039e376ede7fb521bb6552f
andrii <darkness1237893@gmail.com>
2022-07-26 13:27:20 +0300

    styles for header

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -9,36 +9,22 @@ import Header from "../Pages/Header/Header";
 import Login from "../Pages/Login/Login";
 import PageNotFound from "../Pages/PageNotFound/PageNotFound";
 
-interface IconsoleFunc {
-  (input: string): void;
-}
-
-const App = () => {
-  const arr: number[] = [5, 7, 4, 2, 1];
-  const consoleInput: IconsoleFunc = (input: string) => {
-    console.log(input);
-  };
-
-  return (
-    <div className={s.app}>
-      <Header />
-      <main>
-        <Routes>
-          <Route
-            path="/home"
-            element={<Home consoleInput={consoleInput} arr={arr} />}
-          />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/reservation" element={<Reservation />} />
-          <Route path="/personal" element={<Personal />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </main>
-      <footer className={s.footer}>footer</footer>
-    </div>
-  );
-}
+const App = () => (
+  <div className={s.app}>
+    <Header />
+    <main>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/personal" element={<Personal />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </main>
+    <footer className={s.footer}>footer</footer>
+  </div>
+);
 
 export default App;
--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,37 +1,9 @@
-import React, { useState } from "react";
 import s from "./Home.module.scss";
 
-interface IHomeProps {
-  consoleInput: string;
-}
-const Home: React.FC<IHomeProps> = ({ consoleInput }) => {
-  const [searchText, setSearchText] = useState<string>("");
-  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchText(e.target.value);
-  };
-  const keyPressHandler = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter") {
-      consoleInput(searchText);
-      setSearchText("");
-    }
-  };
-
-  return (
-    <div className={s.homeBlock}>
-      <input
-        id="home-search"
-        placeholder="Search something"
-        value={searchText}
-        onChange={changeHandler}
-        onKeyPress={keyPressHandler}
-      />
-      <img
-        className={s.homeImg}
-        src="https://wallpaperaccess.com/full/271687.jpg"
-        alt="Main food"
-      />
-    </div>
-  );
-};
+const Home = () => (
+  <div className={s.homeBlock}>
+    <input id="home-search" placeholder="Search something" />
+  </div>
+);
 
 export default Home;
